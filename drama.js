a15a41215daf7cff02599d747ccd621a7534002a
Hexasphere0 <kesavgurugubelli@gmail.com>
2023-06-24 11:53:26 -0700

    add more people

--- drama.js
+++ drama.js
@@ -11,7 +11,9 @@ let features = [
     "Shaders",
     "Mushroom Tofu",
     "Spice of life",
-    "Optifine Support"
+    "Optifine Support",
+    "Solar panels",
+    "Dayblooms"
 ]
 
 // %& = him her their
@@ -28,42 +30,66 @@ let people = [
         "name": 'Tech22',
         "groups": ["Gregtech CEU discord", "Gregtech Intergalactical discord", "Technological Journey discord", "GTNH discord"],
         "projects": ["GTCEU"],
-        "pronouns": ["him", "his"]
+        "pronouns": ["him", "his", "he"]
     },
     {
         "name": "GregoriousT",
         "groups": ["Gregtech Intergalactial Discord", "IC2 Forums"],
         "projects": ["GT6"],
-        "pronouns": ["him", "his"]
+        "pronouns": ["him", "his", "he"]
     },
     {
         "name": "IGBLON",
         "groups": ["IGBLONcord", "Gregtech CEU discord"],
         "projects": ["Supersymmetry"],
-        "pronouns": ["him", "his"]
+        "pronouns": ["him", "his", "he"]
     },
     {
         "name": "Threefold",
-        "groups": ["Threefold Discord", "Gregtech CEU discord", "Technological Journey discord", "Nomifactory discord"],
+        "groups": ["Threefold discord", "Gregtech CEU discord", "Technological Journey discord", "Nomifactory discord", "DJ2 discord", "Create discord"],
         "projects": ["Trifecta"],
-        "pronouns": ["him", "his"]
+        "pronouns": ["him", "his", "he"]
     },
     {
         "name": "Technici4n",
         "groups": ["Gregtech CEU discord", "MI discord"],
         "projects": ["Modern Industrialization"],
-        "pronouns": ["him", "his"]
+        "pronouns": ["him", "his", "he"]
     },
     {
         "name": "Exa",
         "groups": ["Nomifactory discord", "Gregtech CEU discord"],
         "projects": ["Nomifactory", "GTCE"],
-        "pronouns": ["him", "his"]
+        "pronouns": ["him", "his", "he"]
+    },
+    {
+        "name": "Jellysquid",
+        "groups": ["CaffieneMC discord"],
+        "projects": ["Sodium", "Hytale"],
+        "pronouns": ["her", "her", "she"]
+    },
+    {
+        "name": "Vazkii",
+        "groups": ["CaffieneMC"],
+        "projects": ["Quark", "Botania", "Patchouli", "Psi"],
+        "pronouns": ["him", "his", "he"]
+    },
+    {
+        "name": "Atricos",
+        "groups": ["Threefold discord", "DJ2 discord"],
+        "projects": ["DJ2", "DJ3"],
+        "pronouns": ["him", "his", "he"]
+    },
+    {
+        "name": "Simibubi",
+        "groups": ["Create discord"],
+        "projects": ["Create", "Create Above & Beyond"],
+        "pronouns": ["him", "his", "he"]
     }
 ]
 
 let drama = [
-    ["$NEWNAME", "bans", "$NEWNAME", "from the", "$VAR 0 GROUP", "because he added", "$FEATURE", ".", "$VAR 0", "is banned from", "$GENERIC_GROUP", ".", "$GENERIC_GROUP", "rages", "."],
+    ["$NEWNAME", "bans", "$NEWNAME", "from the", "$VAR 0 GROUP", "because", "$VAR 1 PRONOUNS", "added", "$FEATURE", ".", "$VAR 0", "is banned from", "$GENERIC_GROUP", ".", "$GENERIC_GROUP", "rages", "."],
     ["$NEWNAME", "forks", "$NEWNAME HIDDEN", "$VAR 1 PROJECT", "and adds", "$FEATURE", ".", "$VAR 1", "$SELFDAMAGE 1", "."]
 ]
 
@@ -126,6 +152,9 @@ function genDrama(){
                 if(keyword == "GROUP"){
                     let possibleGroups = variables[index].groups
                     dramaString += possibleGroups[randomInt(0, possibleGroups.length - 1)]
+                }
+                else if(keyword == "PRONOUNS"){
+                    dramaString += variables[index].pronouns[2]
                 }
                 else if(keyword == "PROJECT"){
                     let possibleProjects = variables[index].projects
